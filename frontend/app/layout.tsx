62f07a1874865354b74223fea04a82d652beba57
LukasTr1980 <lukas.troebinger@gmail.com>
2024-08-18 09:05:05 +0200

    Added logo Userleaks. Links improvement for mobile usability.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import Sidebar from './components/Sidebar';
 import NavLinks from './components/Navlinks';
+import logoSmallImage from './image/logo-43x48.webp';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
-  title: 'UserLeaks',
+  title: 'Userleaks',
   description: 'Tracking users',
 };
 
@@ -20,13 +22,20 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <html lang="en">
       <body>
         <div className="grid grid-rows-[auto_1fr] h-screen min-w-full relative">
           <div className="w-full h-16 flex items-center justify-center relative border-b border-gray-300">
             <Sidebar />
-            Userleaks
+            <Image
+              src={logoSmallImage}
+              alt='Userleaks logo'
+              className='hidden sm:block left-4 absolute'
+              width={43}
+              height={48}
+            />
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-[200px_1fr]">
             {/* Sidebar */}
